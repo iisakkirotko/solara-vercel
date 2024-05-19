9f65ee2536013a93ae5efa9d38b1b41b4ef80687
Iisakki Rotko <iisakki.rotko@gmail.com>
2024-05-19 19:08:22 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 
+os.environ["SOLARA_APP"] = "pages/__init__.py"
+
+import uvicorn
 import solara.server.starlette
 from starlette.applications import Starlette
 from starlette.requests import Request
@@ -7,18 +10,10 @@ from starlette.responses import JSONResponse
 from starlette.routing import Route, Mount
 
 
-# os.environ["SOLARA_APP"] = "pages/__init__.py"
+# os.environ["APP_MODULE"] = "main:app"
 
-def myroot(request: Request):
-    return JSONResponse({"framework": "solara"})
-
-routes = [
-    Route("/", myroot),
-    # Mount("/solara", routes=solara.server.starlette.routes)
-]
-
-app = Starlette(routes=routes)
+app = solara.server.starlette.app
 
 if __name__ == "__main__":
-    import uvicorn
+    print("Starting app ", os.environ["SOLARA_APP"])
     uvicorn.run(app, host="0.0.0.0", port=8000)
