97510b9e80a08e684cceb77564435b018e09fc60
Iisakki Rotko <iisakki.rotko@gmail.com>
2024-05-19 19:35:20 +0200

    trial

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import os
 
 os.environ["SOLARA_APP"] = "pages/__init__.py"
 
+from fastapi import FastAPI
 import uvicorn
 import solara.server.starlette
-from starlette.applications import Starlette
-from starlette.requests import Request
-from starlette.responses import JSONResponse
-from starlette.routing import Route, Mount
 
 
-# os.environ["APP_MODULE"] = "main:app"
+app = FastAPI()
 
-app = solara.server.starlette.app
+@app.get("/")
+def read_root():
+    return {"framework": "solara"}
+
+app.mount("/solara", solara.server.starlette.app)
+
+# app = solara.server.starlette.app
 
 if __name__ == "__main__":
     print("Starting app ", os.environ["SOLARA_APP"])
